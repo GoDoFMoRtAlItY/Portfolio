6011ee487b4414cf4cdcc07de2449fda46f35aae
GoDoFMoRtAlItY <ayush369789@gmail.com>
2025-08-07 00:27:47 +0530

    This is my portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn1=document.querySelector("#btn1");
+let btn2=document.querySelector("#btn2");
+let btn3=document.querySelector("#btn3");   
+btn1.addEventListener("click",() => {
+    window.location.href="project.html";
+});
+btn2.addEventListener("click",() => {
+    window.location.href="Anime.html";
+});
+btn3.addEventListener("click",() => {   
+    window.location.href="Games.html";
+});
